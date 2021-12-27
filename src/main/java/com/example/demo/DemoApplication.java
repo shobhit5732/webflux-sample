c013af066fe48a84108dbc5f8c5ca818d199a68b
shobhit5732 <shobhitjaiswal5732@gmail.com>
2021-12-27 06:40:58 -0500

    branch commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,15 +3,20 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@Controller
 public class DemoApplication {
 
+@GetMapping("/hello")
+	void meth()
+	{}
 
 	public static void main(String[] args) {
 
-		Thread.sleep("10000");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
